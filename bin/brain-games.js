7c020ac32a42595a9b6a7b4e7e9f4446c7d772b1
dimong5 <grygymev@mail.ru>
2020-08-27 16:11:54 +0300

    Step#4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import {userGreet} from '../src/cli.js';
+import userGreet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 userGreet();
+console.log('hello');
