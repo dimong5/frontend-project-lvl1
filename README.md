19ab68206ed817157f3f38b786b87f5adeba79d4
dimong5 <grygymev@mail.ru>
2020-09-10 14:14:31 +0300

    Step#7

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <a href="https://codeclimate.com/github/dimong5/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/764945c4b88258bedc67/maintainability" /></a><br>
 ![CI](https://github.com/dimong5/frontend-project-lvl1/workflows/CI/badge.svg) <br>
 [![asciicast](https://asciinema.org/a/gOdXrgE4lhhBiUAOtpFgOqjt7.svg)](https://asciinema.org/a/gOdXrgE4lhhBiUAOtpFgOqjt7) <br>
-[![asciicast](https://asciinema.org/a/zNfeHi0NIgWbXban830szQvnE.svg)](https://asciinema.org/a/zNfeHi0NIgWbXban830szQvnE)
+[![asciicast](https://asciinema.org/a/zNfeHi0NIgWbXban830szQvnE.svg)](https://asciinema.org/a/zNfeHi0NIgWbXban830szQvnE) <br>
+[![asciicast](https://asciinema.org/a/qqlVSlEMUxt8Po1eww6A5dit1.svg)](https://asciinema.org/a/qqlVSlEMUxt8Po1eww6A5dit1) <br>
---
+++ src/gcd.js
@@ -0,0 +1,45 @@
+import pairs from '@hexlet/pairs';
+import engine from './engine.js';
+import getRandom from './utils.js';
+
+const { cons, car, cdr } = pairs;
+const findSmallerNumber = (num1, num2) => {
+  let smallerNumber;
+  let biggerNumber;
+  if (num1 < num2) {
+    smallerNumber = num1;
+    biggerNumber = num2;
+  } else {
+    smallerNumber = num2;
+    biggerNumber = num1;
+  }
+  return cons(smallerNumber, biggerNumber);
+};
+
+const rules = 'Find the greatest common divisor of given numbers.';
+const gcd = () => {
+  const firstNumber = getRandom(1, 50);
+  const secondNumber = getRandom(1, 50);
+  const currentQuestion = `${firstNumber} ${secondNumber}`;
+  let currentAnswer;
+  if (firstNumber === secondNumber) {
+    currentAnswer = firstNumber;
+  } else {
+    const pair = findSmallerNumber(firstNumber, secondNumber);
+    const smallerNumber = car(pair);
+    const biggerNumber = cdr(pair);
+    let divisor = smallerNumber;
+    while (divisor >= 1) {
+      if (((smallerNumber % divisor) === 0) && ((biggerNumber % divisor) === 0)) {
+        currentAnswer = divisor;
+        break;
+      }
+      divisor -= 1;
+    }
+    currentAnswer = divisor;
+  }
+  return cons(currentQuestion, String(currentAnswer));
+};
+export default () => {
+  engine(gcd, rules);
+};
